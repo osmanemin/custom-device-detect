c0c1c44f38c9c28511a6f390591773c846002d68
Yusuf Ulusoy <yusuf.ulusoy@cimri.com>
2022-04-26 17:08:47 +0300

    Add typescript support

---
+++ src/device-detect.ts
@@ -0,0 +1,30 @@
+import mobileAndBotDevices from "./devices";
+
+const dedector = (userAgent: string, myCustomUserAgent: string = "") => {
+  if (typeof userAgent !== "string") return;
+
+  const deviceType = {
+    isDesktop: false,
+    isMobile: false,
+    isBot: false,
+    isCustomDevice: false,
+  };
+
+  const currentDevice = mobileAndBotDevices.find((device) => {
+    const findedDevice = device.deviceRegexes.find((regex) => {
+      return userAgent.match(regex);
+    });
+    return findedDevice;
+  });
+
+  if (userAgent === myCustomUserAgent) deviceType.isCustomDevice = true;
+  else if (currentDevice && currentDevice.name.match(/mobile|tablet/))
+    deviceType.isMobile = true;
+  else deviceType.isDesktop = true;
+
+  if (currentDevice && currentDevice.name.match(/bot/)) deviceType.isBot = true;
+
+  return deviceType;
+};
+
+export default dedector;
---
+++ src/devices.d.ts
@@ -0,0 +1,5 @@
+declare const _default: {
+    name: string;
+    deviceRegexes: RegExp[];
+}[];
+export default _default;
